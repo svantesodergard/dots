67e4b721a6c07f163cfc69c9e338e8c47a4def54
Svante <svante03+github@gmail.com>
2024-12-01 16:18:06 +0200

    Added support for TeX

--- nvim/init.lua
+++ nvim/init.lua
@@ -135,6 +135,7 @@ require('lazy').setup({
     end,
   },
   'windwp/nvim-ts-autotag',
+  'm4xshen/autoclose.nvim',
 
   {
     'jinh0/eyeliner.nvim',
@@ -144,6 +145,18 @@ require('lazy').setup({
       }
     end
   },
+
+  --TeX support
+  {
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "zathura"
+    end
+  },
+  --
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -470,6 +483,8 @@ cmp.setup {
     { name = 'emmet_vim' },
   },
 }
+
+require("autoclose").setup()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
